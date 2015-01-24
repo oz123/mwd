caf976e0b3686ba44fd1a03492e7f070e3faa271
oz123 <nahumoz@gmail.com>
2015-01-24 14:02:00 +0100

    Always set end of string to \0

--- ex17.c
+++ ex17.c
@@ -116,10 +116,11 @@ void Database_set(struct Connection *conn, int id, const char *name, const char 
     addr->set = 1;
     // WARNING: bug, read the "How To Break It" and fix this
     char *res = strncpy(addr->name, name, MAX_DATA);
-    // demonstrate the strncpy bug
+    addr->name[MAX_DATA - 1] = '\0';
     if(!res) die("Name copy failed");
-
+    
     res = strncpy(addr->email, email, MAX_DATA);
+    addr->email[MAX_DATA - 1] = '\0';
     if(!res) die("Email copy failed");
 }
 
